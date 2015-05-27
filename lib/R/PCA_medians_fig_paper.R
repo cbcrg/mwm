07a8946492c54380e8ba7ec042558039347408e1
Jose Espinosa <kadomu@gmail.com>
2015-05-27 11:58:27 +0200

    Adding circle plot

--- lib/R/PCA_medians_fig_paper.R
+++ lib/R/PCA_medians_fig_paper.R
@@ -93,3 +93,358 @@ pca_medians_acq <- ggplot(pca2plot, aes(x=PC1, y=PC2, colour=gentreat )) +
                          
 pca_medians_acq 
 ggsave (pca_medians_acq, file=paste(home, "/20150515_PCA_old_frotiersPaper/figures/", "PCA_medians.jpg", sep=""), dpi=900)
+
+
+
+
+
+
+############################
+
+mWT=rbind(M1[treat1=="WT",],M2[treat2=="WT",],M3[treat3=="WT",],M4[treat4=="WT",],M[treat=="WT",])
+
+sMMALL=matrix(0,dim(MMALL)[1],dim(MMALL)[2])
+for (i in 1:dim(MMALL)[1]){
+  sMMALL[i,]=(MMALL[i,]-apply(MMALL,2,mean))/apply(MMALL,2,sd)
+}
+
+#this is the same scaling that is employed when doing prcomp
+
+#Now: we have to use these means and variances for the original data
+smWT=matrix(0,dim(mWT)[1],dim(mWT)[2])
+for (i in 1:dim(mWT)[1]){
+  smWT[i,]=(mWT[i,]-apply(MMALL,2,mean))/apply(MMALL,2,sd)
+}
+#check:
+apply(smWT[1:10,],2,median)%*%ResMMALL$rotation[,1]==ResMMALL$x[1,1]
+apply(smWT[11:20,],2,median)%*%ResMMALL$rotation[,1]==ResMMALL$x[9,1]
+
+############################################################################################################
+
+#plot all data as supplementary points
+
+acq=c("Day 1","Day 2","Day 3","Day 4","Day 5")
+var=c(4,5,7,8,9,11,12)
+tgt=sort(unique(iddaytreat[,2]))
+
+M.ind=as.matrix(ma2[ma2$day%in%acq,var])
+iddaytreat=as.matrix(ma2[ma2$day%in%acq,1:3])
+rownames(iddaytreat)=c(1:415)
+
+M.med=matrix(0,40,7)
+rnames=c(1:40)
+for (i in 1:length(acq)){
+  for (j in 1:length(tgt)){
+    ind=(i-1)*length(tgt)+j
+    print (ind)
+    M.med[ind,]=apply(M.ind[iddaytreat[,3]==acq[i] & iddaytreat[,2]==tgt[j],],2,median)
+    rnames[ind]=paste(tgt[j],acq[i]," ")
+  }
+}
+colnames(M.med)=colnames(M.ind)
+rownames(M.med)=rnames
+
+jm=rbind(M.med,M.ind)
+
+
+library(FactoMineR)
+res = PCA(jm, scale.unit=TRUE, ind.sup=c(41:455)) 
+plot(res,choix="var")
+cols=c("green","lightblue","black","orange","red","blue","darkgrey","magenta")
+
+
+png("figures/PCAmed_variables.png",res=300,width=15,height=15,unit="cm")
+plot(res$var$coord[,1],res$var$coord[,2],asp=1,ylim=c(-1.1,1.1),type="n",main="Mapped variables",xlab="PC1 (80% of variance)",ylab="PC2 (12% of variance)")
+arrows(0,0,-res$var$coord[,1],-res$var$coord[,2],length=0.1,angle=10)
+textxy(-res$var$coord[,1],-res$var$coord[,2],colnames(M.med),cex=0.9,offset=0.5,col="red")
+circle(1,c(0,0))
+origin(lty=2)
+dev.off()
+
+row.names(res$var$coord)
+circle_plot <- as.data.frame (res$var$coord)
+circle_plot$labels <- row.names(res$var$coord)
+labels_v <- row.names(res$var$coord)
+neg_labels <- labels_v [c(1,2,3,7)]
+neg_positions <- circle_plot [c(1,2,3,7), c(1,2)]
+# change positions for labels
+neg_positions [2,2] <-neg_positions [2,2] + 0.04 
+neg_positions [3,2] <-neg_positions [3,2] - 0.04
+pos_labels <- labels_v [c(4,5,6)]
+pos_positions <- circle_plot [c(4,5,6), c(1,2)]
+
+#aes(x=PC1, y=PC2, colour=gentreat )) 
+circle_plot <- ggplot(circle_plot) + 
+                       geom_segment (data=circle_plot, aes(x=0, y=0, xend=-Dim.1, yend=-Dim.2), arrow=arrow(length=unit(0.2,"cm")), alpha=1, size=1, color="red") +
+                       xlim (c(-1.2, 1.2)) + ylim (c(-1.2, 1.2)) +
+                       geom_text (data=neg_positions, aes (x=-Dim.1, y=-Dim.2, label=neg_labels, hjust=1.2), show_guide = FALSE, size=5) + 
+                       geom_text (data=pos_positions, aes (x=-Dim.1, y=-Dim.2, label=pos_labels, hjust=-0.3), show_guide = FALSE, size=5) +
+                       geom_vline (xintercept = 0, linetype="dotted") +
+                       geom_hline (yintercept=0, linetype="dotted") +
+                       labs (title = "Variable contributions\n", x = "\nPC1 (80% of variance)", y="PC2 (12% of variance)\n") +
+                #        geom_polygon(aes(x, y), data = df, inherit.aes = F, Fill=NA)
+                       geom_polygon (data = df, aes(x, y), alpha=1,colour="black", fill=NA, size=1)
+circle_plot
+ggsave (circle_plot, file=paste(home, "/20150515_PCA_old_frotiersPaper/figures/", "circle_plot.jpg", sep=""), width = 10, height = 10, dpi=900)
+
+
+
+
+
+
+vjust=-0.5, hjust=1,
+
+
+png("figures/PCAmed_varcontr1.png",res=300,width=15,height=7.5,unit="cm")
+barplot(sort(res$var$coord[,1]^2/sum(res$var$coord[,1]^2)*100,decreasing=TRUE),names=names(res$var$coord)[order(res$var$coord[,1]^2),decreasing=TRUE],cex.names=0.7,ylab="contribution in %",main="Variable contributions to PC1")
+dev.off()
+png("figures/PCAmed_varcontr2.png",res=300,width=15,height=7.5,unit="cm")
+barplot(sort(res$var$coord[,2]^2/sum(res$var$coord[,2]^2)*100,decreasing=TRUE),names=names(res$var$coord)[order(res$var$coord[,1]^2),decreasing=TRUE],cex.names=0.7,ylab="contribution in %",main="Variable contributions to PC2")
+dev.off()
+
+
+png("figures/PCAmed_medians.png",res=300,width=15,height=15,unit="cm")
+plot(-res$ind$coord[,1],-res$ind$coord[,2],type="n",xlim=c(-4.2,4),main="PCA of group medians",xlab="PC1 (80% of variance)",ylab="PC2 (12% of variance)")
+for (j in 1:length(tgt)){
+  for (i in 1:length(acq)){
+    ind=(i-1)*length(tgt)+j
+    textxy(-res.pca$ind$coord[c(ind,ind2),1],-res.pca$ind$coord[c(ind,ind2),2],i,col=cols[j],cex=0.8)
+    if (i<5){
+      ind2=i*length(tgt)+j
+      lines(-res.pca$ind$coord[c(ind,ind2),1],-res.pca$ind$coord[c(ind,ind2),2],col=cols[j])
+    }
+    
+  }
+}
+legend(x="bottomleft",c("WT","TS","WTEE","TSEE","WTEGCG","TSEGCG","WTEEEGCG","TSEEEGCG"),col=c("red","green","blue","lightblue","magenta","orange","grey","black"),lty=1,cex=0.6)
+dev.off()
+
+png("figures/PCAmed_individuals.png",res=300,width=15,height=15,unit="cm")
+plot(-res$ind.sup$coord[,1],-res$ind.sup$coord[,2],type="n",main="Individual variation as supplementary points",xlab="PC1 (80% of variance)",ylab="PC2 (12% of variance)")
+for (j in 1:length(tgt)){
+  for (i in 1:length(acq)){
+    if (i<5){
+      ind=(i-1)*length(tgt)+j
+      ind2=i*length(tgt)+j
+      lines(-res.pca$ind$coord[c(ind,ind2),1],-res.pca$ind$coord[c(ind,ind2),2],col=cols[j])
+    }
+    myset=which(iddaytreat[,2]==tgt[j] & iddaytreat[,3]==acq[i])
+    text(-res.pca$ind.sup$coord[myset,1],-res.pca$ind.sup$coord[myset,2],i,col=cols[j],cex=0.5)
+  }
+}
+dev.off()
+
+
+
+
+#permutation test:
+di=2
+for (a in 1:5){
+  set1=which(iddaytreat[,2]=="TS" & iddaytreat[,3]==acq[a])
+  set2=which(iddaytreat[,2]=="TSEGCG" & iddaytreat[,3]==acq[a])
+  M1=mean(res.pca$ind.sup$coord[set1,di])
+  M2=mean(res.pca$ind.sup$coord[set2,di])
+  m=length(set1)
+  n=length(set2)
+  S=sqrt((sum((res.pca$ind.sup$coord[set1,di]-M1)^2)+sum((res.pca$ind.sup$coord[set1,di]-M2)^2))/(m+n-2))
+  truestat=((M1-M2)*sqrt(m*n))/(S*sqrt(m+n))
+  
+  set=c(set1,set2)
+  set.seed(317)
+  st=c(1:10000)
+  st[1]=truestat
+  for (i in 2:10000){
+    #print(i)
+    sam=sample(set)
+    set1=sam[1:m]
+    set2=sam[(m+1):(m+n)]
+    M1=mean(res.pca$ind.sup$coord[set1,di])
+    M2=mean(res.pca$ind.sup$coord[set2,di])
+    S=sqrt((sum((res.pca$ind.sup$coord[set1,di]-M1)^2)+sum((res.pca$ind.sup$coord[set1,di]-M2)^2))/(m+n-2))
+    stat=((M1-M2)*sqrt(m*n))/(S*sqrt(m+n))
+    st[i]=stat
+  }
+  p=max(which (sort(st,decreasing=TRUE)==st[1])/10000)
+  print (acq[a])
+  print (p)
+}
+
+#R objects for Jose:
+
+set=which(iddaytreat[,2]=="TS" & iddaytreat[,3]==acq[a])
+m=length(set)
+s1=matrix(0,m,5)
+for (a in 1:5){
+  set=which(iddaytreat[,2]=="TS" & iddaytreat[,3]==acq[a])
+  s1[,a]=-res.pca$ind.sup$coord[set,1]
+}
+
+set=which(iddaytreat[,2]=="TSEEEGCG" & iddaytreat[,3]==acq[a])
+n=length(set)
+s2=matrix(0,n,5)
+for (a in 1:5){
+  set=which(iddaytreat[,2]=="TSEEEGCG" & iddaytreat[,3]==acq[a])
+  s2[,a]=-res.pca$ind.sup$coord[set,1]
+}
+
+set=which(iddaytreat[,2]=="TSEE" & iddaytreat[,3]==acq[a])
+n=length(set)
+s3=matrix(0,n,5)
+for (a in 1:5){
+  set=which(iddaytreat[,2]=="TSEE" & iddaytreat[,3]==acq[a])
+  s3[,a]=-res.pca$ind.sup$coord[set,1]
+}
+
+set=which(iddaytreat[,2]=="TSEGCG" & iddaytreat[,3]==acq[a])
+n=length(set)
+s4=matrix(0,n,5)
+for (a in 1:5){
+  set=which(iddaytreat[,2]=="TSEGCG" & iddaytreat[,3]==acq[a])
+  s4[,a]=-res.pca$ind.sup$coord[set,1]
+}
+
+set=which(iddaytreat[,2]=="WT" & iddaytreat[,3]==acq[a])
+n=length(set)
+s5=matrix(0,n,5)
+for (a in 1:5){
+  set=which(iddaytreat[,2]=="WT" & iddaytreat[,3]==acq[a])
+  s5[,a]=-res.pca$ind.sup$coord[set,1]
+}
+
+TS=s1
+TSEEEGCG=s2
+TSEE=s3
+TSEGCG=s4
+WT=s5
+save(TS,TSEEEGCG,TSEE,TSEGCG,WT,file="5setsPC1.R")
+
+set=which(iddaytreat[,2]=="WTEE" & iddaytreat[,3]==acq[a])
+n=length(set)
+s6=matrix(0,n,5)
+for (a in 1:5){
+  set=which(iddaytreat[,2]=="WTEE" & iddaytreat[,3]==acq[a])
+  s6[,a]=-res.pca$ind.sup$coord[set,1]
+}
+
+WTEE=s6
+
+set=which(iddaytreat[,2]=="WTEGCG" & iddaytreat[,3]==acq[a])
+n=length(set)
+s7=matrix(0,n,5)
+for (a in 1:5){
+  set=which(iddaytreat[,2]=="WTEGCG" & iddaytreat[,3]==acq[a])
+  s7[,a]=-res.pca$ind.sup$coord[set,1]
+}
+
+WTEGCG=s7
+
+set=which(iddaytreat[,2]=="WTEEEGCG" & iddaytreat[,3]==acq[a])
+n=length(set)
+s8=matrix(0,n,5)
+for (a in 1:5){
+  set=which(iddaytreat[,2]=="WTEEEGCG" & iddaytreat[,3]==acq[a])
+  s8[,a]=-res.pca$ind.sup$coord[set,1]
+}
+
+WTEEEGCG=s8
+
+save(WTEEEGCG,WTEE,WTEGCG,file="3setsPC1.R")
+
+
+
+
+boxplot(s1,ylim=c(-6.2,7.2),ylab="PC1",xlab="Acquisition day")
+boxplot(s2,add=TRUE,col=rgb(0,1,0,0.5))
+legend("topleft",pch=0,col=c("green","white"),c("TS","TSEEEGCG"))
+
+
+
+
+
+#analyse removal session:
+
+ma3=spss.get("Jtracks parameters except latency.sav")
+MR=ma3[1:83,c("DIST.REM","GALLINDEX.REM","SPEED.REM","PERC.NE.REM","PERC.PERI.REM","WISHAW.REM")]
+gt=sort(unique(idgentreat[,2]))
+idgentreat=ma3[1:83,c("Mouse.ID","GEN.TREAT")]
+
+MR.med=matrix(0,8,6)
+for (i in 1:length(gt)){
+  print (i)
+  MR.med[i,]=apply(MR[idgentreat[1:83,2]==gt[i],],2,median)
+  
+}
+colnames(MR.med)=colnames(MR)
+rownames(MR.med)=gt
+
+joint=rbind(MR.med,MR)
+resR = PCA(joint, scale.unit=TRUE, ind.sup=c(9:91))
+
+#permutation test:
+di=3
+set1=which(idgentreat[,2]=="TS")
+set2=which(idgentreat[,2]=="TSEEEGCG")
+M1=mean(resR$ind.sup$coord[set1,di])
+M2=mean(resR$ind.sup$coord[set2,di])
+m=length(set1)
+n=length(set2)
+S=sqrt((sum((resR$ind.sup$coord[set1,di]-M1)^2)+sum((resR$ind.sup$coord[set1,di]-M2)^2))/(m+n-2))
+truestat=((M1-M2)*sqrt(m*n))/(S*sqrt(m+n))
+
+set=c(set1,set2)
+set.seed(317)
+st=c(1:10000)
+st[1]=truestat
+for (i in 2:10000){
+  #print(i)
+  sam=sample(set)
+  set1=sam[1:m]
+  set2=sam[(m+1):(m+n)]
+  M1=mean(resR$ind.sup$coord[set1,di])
+  M2=mean(resR$ind.sup$coord[set2,di])
+  S=sqrt((sum((resR$ind.sup$coord[set1,di]-M1)^2)+sum((resR$ind.sup$coord[set1,di]-M2)^2))/(m+n-2))
+  stat=((M1-M2)*sqrt(m*n))/(S*sqrt(m+n))
+  st[i]=stat
+}
+p=max(which (sort(st)==st[1])/10000)
+print (p)
+
+
+#check original variables: Gallagher index: 
+
+a=4
+set1=which(iddaytreat[,2]=="TS" & iddaytreat[,3]==acq[a])
+set2=which(iddaytreat[,2]=="TSEEEGCG" & iddaytreat[,3]==acq[a])
+GA1=as.numeric(M.ind[set1,"gallindex"])
+GA2=as.numeric(M.ind[set2,"gallindex"])
+
+a=4
+set1=which(iddaytreat[,2]=="TS" & iddaytreat[,3]==acq[a])
+set2=which(iddaytreat[,2]=="TSEEEGCG" & iddaytreat[,3]==acq[a])
+LA1=as.numeric(M.ind[set1,"latency"])
+LA2=as.numeric(M.ind[set2,"latency"])
+
+
+#discriminant analysis trisomic:
+
+M.ind=as.matrix(ma2[ma2$day%in%acq,var])
+iddaytreat=as.matrix(ma2[ma2$day%in%acq,1:3])
+rownames(iddaytreat)=c(1:415)
+
+M.med=matrix(0,40,7)
+rnames=c(1:40)
+for (i in 1:length(acq)){
+  for (j in 1:length(tgt)){
+    ind=(i-1)*length(tgt)+j
+    print (ind)
+    M.med[ind,]=apply(M.ind[iddaytreat[,3]==acq[i] & iddaytreat[,2]==tgt[j],],2,median)
+    rnames[ind]=paste(tgt[j],acq[i]," ")
+  }
+}
+colnames(M.med)=colnames(M.ind)
+rownames(M.med)=rnames
+
+jm=rbind(M.med,M.ind)
+
+
+
