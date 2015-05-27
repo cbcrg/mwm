cdfe617412bf82b8bc1edf3b4b9f082451b88291
Jose Espinosa <kadomu@gmail.com>
2015-05-28 00:09:10 +0200

    Plot individual values PCA and stats

--- lib/R/removal_PCA.R
+++ lib/R/removal_PCA.R
@@ -5,6 +5,8 @@
 ### Modified from Ionas script called PCA_clean for PCA analysis          ### 
 #############################################################################
 
+source (paste (home, "/git/mwm/lib/R/plot_param_public.R", sep=""))
+
 # Calling libraries
 library(Hmisc)
 library(calibrate)
@@ -62,6 +64,64 @@ g_genotype_tt <- ggplot(pca_rem_2plot, aes(PC1_neg, PC2)) + geom_point(aes(colou
 
 g_genotype_tt
 
+#################################
+# Individual values for boxPlot
+# install.packages("FactoMineR")
+library(FactoMineR)
+tbl_ind_rem <- rem_data_all_var[,c(7:length(rem_data_all_var[1,]))]
+tbl_ind_rem_med<- rbind (tbl_stat_median, tbl_ind_rem)
+
+res_pca_rem = PCA(tbl_ind_rem_med, scale.unit=TRUE, ind.sup=c(9:91))
+
+new_coord_rem <- cbind(res_pca_rem$ind.sup$coord[,1],res_pca_rem$ind.sup$coord[,2])
+new_coord_rem <- as.data.frame(new_coord_rem)
+new_coord_rem$genotype <- rem_data_all_var[,c(6)]
+new_coord_rem$genotype <- gsub("H20", "", new_coord_rem$genotype)
+new_coord_rem$genotype <- gsub("NE", "", new_coord_rem$genotype)
+new_coord_rem$genotype <- factor(new_coord_rem$genotype , levels=c("WT", "TS", "WTEE", "TSEE", "WTEGCG", "TSEGCG", "WTEEEGCG", "TSEEEGCG"), 
+                             labels=c("WT", "TS", "WTEE", "TSEE", "WTEGCG", "TSEGCG", "WTEEEGCG", "TSEEEGCG"))
+
+pca_plot_individuals_rem <- ggplot (data=new_coord_rem, aes (V1, V2)) + 
+#   geom_text (aes(label=day, colour = genotype), size=3, show_guide = FALSE) +
+  geom_point () 
+
+#PLOT_paper
+ggsave (pca_plot_individuals, file=paste(home, "/20150515_PCA_old_frotiersPaper/figures/", "PCA_individuals.jpg", sep=""), dpi=900)
+
+# ANOVA
+new_coord_rem_TS <- subset(new_coord_rem, grepl("TS", new_coord_rem$genotype))
+new_coord_rem_TS$id <- c(1:length(new_coord_rem_TS$V1))
+rem.aov <- aov(V1 ~ genotype  + Error(id), data = new_coord_rem_TS)
+summary(rem.aov)
+
+# Post-hoc
+# df.anova$interaction <- paste(df.anova$group, df.anova$time, sep="_")
+pairwise.t.test(new_coord_rem$V1, new_coord_rem$genotype, , p.adj="hochberg", paired=F)
+
+# boxplot
+boxPlots_rem <- ggplot(new_coord_rem_TS , aes (genotype, V1, fill = genotype)) + 
+  geom_boxplot(show_guide=FALSE) +
+  scale_fill_manual(name = "genotype", values=c("green", "lightblue", "orange", "black")) +
+  labs(title = "Removal PC1\n") + xlab ("\ngentreat") + ylab("PC1\n") +
+  theme (legend.title=element_blank())
+
+
+boxPlots_rem
+
+
+boxPlots_rem
++ 
+  scale_y_continuous(breaks=c(-4,-2,0,2,4,6,8), limits=c(-5.5, 9.5))
+  
+  scale_color_manual(values = c("black", "brown")) +
+  scale_fill_manual(name = "Group", values = c("green", "black"), labels = c ("TS", "TSEEEGCG")) +
+  #scale_fill_manual (name = "Group", values = c ("green", "brown")) +
+  labs(title = "") + xlab ("\nAcquisition days") + ylab("PC1\n")
+
+
+plot(res,choix="var")
+cols=c("green","lightblue","black","orange","red","blue","darkgrey","magenta")
+
 
 
 
@@ -78,24 +138,24 @@ res_rem <- prcomp(tbl_stat_median, scale=TRUE)
 summary(res_rem)
 res_rem$x
 # png("figures/PCAmedians.png",res=300,width=15,height=15,unit="cm")
-dev.off()
-plot(res_rem$x[,1],res_rem$x[,2],type="n",main="PCA of group medians",xlab="PA1 (97% of variance)",ylab="PA2 (2% of variance)")
-points(res_rem$x[1,1],res_rem$x[1,2],col="red", pch=21, bg="red")
-textxy(res_rem$x[1,1],res_rem$x[1,2], row.names(res_rem$x)[1], cex=0.7,col="black")
-points(res_rem$x[2,1],res_rem$x[2,2],col="green", pch=21, bg="green")
-textxy(res_rem$x[2,1]+2,res_rem$x[2,2]+0.1, row.names(res_rem$x)[2], cex=0.7,col="black")
-points(res_rem$x[3,1],res_rem$x[3,2],col="yellow", pch=21, bg="yellow")
-textxy(res_rem$x[3,1]-0.7,res_rem$x[3,2]+0.1, row.names(res_rem$x)[3], cex=0.7,col="black")
-points(res_rem$x[4,1],res_rem$x[4,2],col="yellow", pch=21, bg="yellow")
-textxy(res_rem$x[4,1],res_rem$x[4,2], row.names(res_rem$x)[4], cex=0.7,col="black")
-points(res_rem$x[5,1],res_rem$x[5,2],col="magenta", pch=21, bg="magenta")
-textxy(res_rem$x[5,1]-0.4,res_rem$x[5,2], row.names(res_rem$x)[5], cex=0.7,col="black")
-points(res_rem$x[6,1],res_rem$x[6,2],col="magenta", pch=21, bg="lightblue")
-textxy(res_rem$x[6,1]+0.5,res_rem$x[6,2], row.names(res_rem$x)[6], cex=0.7,col="black")
-points(res_rem$x[7,1],res_rem$x[7,2],col="blue", pch=21, bg="blue")
-textxy(res_rem$x[7,1]-3.5,res_rem$x[7,2]+0.05, row.names(res_rem$x)[7], cex=0.7,col="black")
-points(res_rem$x[8,1],res_rem$x[8,2],col="gray", pch=21, bg="gray")
-textxy(res_rem$x[8,1]+1,res_rem$x[8,2]-0.05, row.names(res_rem$x)[8], cex=0.7,col="black")
+# dev.off()
+# plot(res_rem$x[,1],res_rem$x[,2],type="n",main="PCA of group medians",xlab="PA1 (97% of variance)",ylab="PA2 (2% of variance)")
+# points(res_rem$x[1,1],res_rem$x[1,2],col="red", pch=21, bg="red")
+# textxy(res_rem$x[1,1],res_rem$x[1,2], row.names(res_rem$x)[1], cex=0.7,col="black")
+# points(res_rem$x[2,1],res_rem$x[2,2],col="green", pch=21, bg="green")
+# textxy(res_rem$x[2,1]+2,res_rem$x[2,2]+0.1, row.names(res_rem$x)[2], cex=0.7,col="black")
+# points(res_rem$x[3,1],res_rem$x[3,2],col="yellow", pch=21, bg="yellow")
+# textxy(res_rem$x[3,1]-0.7,res_rem$x[3,2]+0.1, row.names(res_rem$x)[3], cex=0.7,col="black")
+# points(res_rem$x[4,1],res_rem$x[4,2],col="yellow", pch=21, bg="yellow")
+# textxy(res_rem$x[4,1],res_rem$x[4,2], row.names(res_rem$x)[4], cex=0.7,col="black")
+# points(res_rem$x[5,1],res_rem$x[5,2],col="magenta", pch=21, bg="magenta")
+# textxy(res_rem$x[5,1]-0.4,res_rem$x[5,2], row.names(res_rem$x)[5], cex=0.7,col="black")
+# points(res_rem$x[6,1],res_rem$x[6,2],col="magenta", pch=21, bg="lightblue")
+# textxy(res_rem$x[6,1]+0.5,res_rem$x[6,2], row.names(res_rem$x)[6], cex=0.7,col="black")
+# points(res_rem$x[7,1],res_rem$x[7,2],col="blue", pch=21, bg="blue")
+# textxy(res_rem$x[7,1]-3.5,res_rem$x[7,2]+0.05, row.names(res_rem$x)[7], cex=0.7,col="black")
+# points(res_rem$x[8,1],res_rem$x[8,2],col="gray", pch=21, bg="gray")
+# textxy(res_rem$x[8,1]+1,res_rem$x[8,2]-0.05, row.names(res_rem$x)[8], cex=0.7,col="black")
 
 # Plot PCA color by genotype
 pca_rem_2plot <- as.data.frame (res_rem$x)
@@ -132,48 +192,48 @@ PCbiplot <- function(PC, x="PC1", y="PC2") {
 
 PCbiplot(res_rem)
 
-textxy(res_rem$x[2,1],res_rem$x[2,2], row.names(res_rem$x)[1], cex=0.7,col="green")
-lines(res_rem$x[2,1], res_rem$x[2,2],col="black")
-
-textxy(res_rem$x[c(5,13,21,29,37),1],res_rem$x[c(5,13,21,29,37),2],c(1:5),cex=0.7,col="green")
-lines(res_rem$x[c(5,13,21,29,37),1],res_rem$x[c(5,13,21,29,37),2],col="green")
-textxy(res_rem$x[c(7,15,23,31,39),1],res_rem$x[c(7,15,23,31,39),2],c(1:5),cex=0.7,col="yellow")
-lines(res_rem$x[c(7,15,23,31,39),1],res_rem$x[c(7,15,23,31,39),2],col="yellow")
-textxy(res_rem$x[c(3,11,19,27,35),1],res_rem$x[c(3,11,19,27,35),2],c(1:5),cex=0.7,col="magenta")
-lines(res_rem$x[c(3,11,19,27,35),1],res_rem$x[c(3,11,19,27,35),2],col="magenta")
-textxy(res_rem$x[c(6,14,22,30,38),1],res_rem$x[c(6,14,22,30,38),2],c(1:5),cex=0.7,col="lightblue")
-lines(res_rem$x[c(6,14,22,30,38),1],res_rem$x[c(6,14,22,30,38),2],col="lightblue")
-textxy(res_rem$x[c(2,10,18,26,34),1],res_rem$x[c(2,10,18,26,34),2],c(1:5),cex=0.7,col="blue")
-lines(res_rem$x[c(2,10,18,26,34),1],res_rem$x[c(2,10,18,26,34),2],col="blue")
-textxy(res_rem$x[c(4,12,20,28,36),1],res_rem$x[c(4,12,20,28,36),2],c(1:5),cex=0.7,col="blue")
-lines(res_rem$x[c(4,12,20,28,36),1],res_rem$x[c(4,12,20,28,36),2],col="grey")
-legend(x="bottomleft",c("WT","TS","WTEE","TSEE","WTEGCG","TSEGCG","WTEEEGCG","TSEEEGCG"),col=c("red","green","blue","lightblue","magenta","yellow","grey","black"),lty=1,cex=0.6)
-altnamesM=c("dist","","latency\ngallindex","speed","percentne","percenter","wishaw","percentperi")
-dev.off()
-png("figures/PCAmedians_variables.png",res=300,width=15,height=15,unit="cm")
-plot(res_rem$rotation[,1],res_rem$rotation[,2],type="n",xlim=c(-1,1),asp=1,main="Variable contributions to principal axes",xlab="PA1 (80% of variance)",ylab="PA2 (11% of variance)")
-arrows(0,0,res_rem$rotation[,1],res_rem$rotation[,2],angle=5,length=0.1)
-textxy(res_rem$rotation[,1],res_rem$rotation[,2],altnamesM,cex=1)
-dev.off()
-
-
-
-
-
-ResMMALL=prcomp(MMall,scale=TRUE)
-
-MM5[1,]=apply(M[treat=="WT",],2,median)
-
-M=as.matrix(ma2[rem_data$day=="Day 5",c(4,5,7,8,9,10,11,12)])
-treat=as.matrix(ma2[ma2$day=="Day 5",2])
-M4=as.matrix(ma2[ma2$day=="Day 4",c(4,5,7,8,9,10,11,12)])
-treat4=as.matrix(ma2[ma2$day=="Day 4",2])
-M3=as.matrix(ma2[ma2$day=="Day 3",c(4,5,7,8,9,10,11,12)])
-treat3=as.matrix(ma2[ma2$day=="Day 3",2])
-M2=as.matrix(ma2[ma2$day=="Day 2",c(4,5,7,8,9,10,11,12)])
-treat2=as.matrix(ma2[ma2$day=="Day 2",2])
-M1=as.matrix(ma2[ma2$day=="Day 1",c(4,5,7,8,9,10,11,12)])
-treat1=as.matrix(ma2[ma2$day=="Day 1",2])
-
-
-
+# textxy(res_rem$x[2,1],res_rem$x[2,2], row.names(res_rem$x)[1], cex=0.7,col="green")
+# lines(res_rem$x[2,1], res_rem$x[2,2],col="black")
+# 
+# textxy(res_rem$x[c(5,13,21,29,37),1],res_rem$x[c(5,13,21,29,37),2],c(1:5),cex=0.7,col="green")
+# lines(res_rem$x[c(5,13,21,29,37),1],res_rem$x[c(5,13,21,29,37),2],col="green")
+# textxy(res_rem$x[c(7,15,23,31,39),1],res_rem$x[c(7,15,23,31,39),2],c(1:5),cex=0.7,col="yellow")
+# lines(res_rem$x[c(7,15,23,31,39),1],res_rem$x[c(7,15,23,31,39),2],col="yellow")
+# textxy(res_rem$x[c(3,11,19,27,35),1],res_rem$x[c(3,11,19,27,35),2],c(1:5),cex=0.7,col="magenta")
+# lines(res_rem$x[c(3,11,19,27,35),1],res_rem$x[c(3,11,19,27,35),2],col="magenta")
+# textxy(res_rem$x[c(6,14,22,30,38),1],res_rem$x[c(6,14,22,30,38),2],c(1:5),cex=0.7,col="lightblue")
+# lines(res_rem$x[c(6,14,22,30,38),1],res_rem$x[c(6,14,22,30,38),2],col="lightblue")
+# textxy(res_rem$x[c(2,10,18,26,34),1],res_rem$x[c(2,10,18,26,34),2],c(1:5),cex=0.7,col="blue")
+# lines(res_rem$x[c(2,10,18,26,34),1],res_rem$x[c(2,10,18,26,34),2],col="blue")
+# textxy(res_rem$x[c(4,12,20,28,36),1],res_rem$x[c(4,12,20,28,36),2],c(1:5),cex=0.7,col="blue")
+# lines(res_rem$x[c(4,12,20,28,36),1],res_rem$x[c(4,12,20,28,36),2],col="grey")
+# legend(x="bottomleft",c("WT","TS","WTEE","TSEE","WTEGCG","TSEGCG","WTEEEGCG","TSEEEGCG"),col=c("red","green","blue","lightblue","magenta","yellow","grey","black"),lty=1,cex=0.6)
+# altnamesM=c("dist","","latency\ngallindex","speed","percentne","percenter","wishaw","percentperi")
+# dev.off()
+# png("figures/PCAmedians_variables.png",res=300,width=15,height=15,unit="cm")
+# plot(res_rem$rotation[,1],res_rem$rotation[,2],type="n",xlim=c(-1,1),asp=1,main="Variable contributions to principal axes",xlab="PA1 (80% of variance)",ylab="PA2 (11% of variance)")
+# arrows(0,0,res_rem$rotation[,1],res_rem$rotation[,2],angle=5,length=0.1)
+# textxy(res_rem$rotation[,1],res_rem$rotation[,2],altnamesM,cex=1)
+# dev.off()
+# 
+# 
+# 
+# 
+# 
+# ResMMALL=prcomp(MMall,scale=TRUE)
+# 
+# MM5[1,]=apply(M[treat=="WT",],2,median)
+# 
+# M=as.matrix(ma2[rem_data$day=="Day 5",c(4,5,7,8,9,10,11,12)])
+# treat=as.matrix(ma2[ma2$day=="Day 5",2])
+# M4=as.matrix(ma2[ma2$day=="Day 4",c(4,5,7,8,9,10,11,12)])
+# treat4=as.matrix(ma2[ma2$day=="Day 4",2])
+# M3=as.matrix(ma2[ma2$day=="Day 3",c(4,5,7,8,9,10,11,12)])
+# treat3=as.matrix(ma2[ma2$day=="Day 3",2])
+# M2=as.matrix(ma2[ma2$day=="Day 2",c(4,5,7,8,9,10,11,12)])
+# treat2=as.matrix(ma2[ma2$day=="Day 2",2])
+# M1=as.matrix(ma2[ma2$day=="Day 1",c(4,5,7,8,9,10,11,12)])
+# treat1=as.matrix(ma2[ma2$day=="Day 1",2])
+# 
+# 
+# 
