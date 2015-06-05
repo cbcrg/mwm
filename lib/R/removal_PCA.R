cd7cc4012702378fb717b311921c1a4f2b0f697f
Jose Espinosa <kadomu@gmail.com>
2015-06-05 14:39:32 +0200

    Removal with selected values

--- lib/R/removal_PCA.R
+++ lib/R/removal_PCA.R
@@ -39,6 +39,8 @@ rem_data_all_var <- merge(rem_data, ma3_filt_rem_data, all=TRUE)
 # Guys starting with 1400277xx are missing in the second table, thus when merging they appear as NA, I delete them
 rem_data_all_var <- head (rem_data_all_var, -5)
 
+head (rem_data_all_var)
+
 # Median with all available rem variables
 # tbl_stat_median <-with (rem_data_all_var, aggregate (cbind (NUMBER.ENTRIES, PERM.TIME, PERCENT.PERM.TIME, LATENCY.TARGET), list (GENTREAT), FUN=function (x) median=median(x)))
 tbl_stat_median <-with (rem_data_all_var, aggregate (cbind (NUMBER.ENTRIES, PERM.TIME, PERCENT.PERM.TIME, LATENCY.TARGET, DIST.REM, GALLINDEX.REM, GALLDIST.REM, SPEED.REM, PERC.NE.REM, PERC.CENTRE.REM, PERC.PERI.REM, WISHAW.REM), list (GENTREAT), FUN=function (x) median=median(x)))
@@ -86,7 +88,7 @@ pca_plot_individuals_rem <- ggplot (data=new_coord_rem, aes (V1, V2)) +
   geom_point () 
 
 #PLOT_paper
-ggsave (pca_plot_individuals, file=paste(home, "/20150515_PCA_old_frotiersPaper/figures/", "PCA_individuals.jpg", sep=""), dpi=900)
+# ggsave (pca_plot_individuals_rem, file=paste(home, "/20150515_PCA_old_frotiersPaper/figures/", "PCA_individuals_rem.jpg", sep=""), dpi=900)
 
 # ANOVA
 new_coord_rem_TS <- subset(new_coord_rem, grepl("TS", new_coord_rem$genotype))
@@ -98,6 +100,13 @@ summary(rem.aov)
 # df.anova$interaction <- paste(df.anova$group, df.anova$time, sep="_")
 pairwise.t.test(new_coord_rem$V1, new_coord_rem$genotype, , p.adj="hochberg", paired=F)
 
+# TS vs TSEEEGCG 
+new_coord_rem_old_var_TS_TSEEEGCG <- subset(new_coord_rem, genotype == "TS" | genotype == "TSEEEGCG")
+new_coord_rem_old_var_TS_TSEEEGCG$id <- c(1:length(new_coord_rem_old_var_TS_TSEEEGCG$V1))
+new_coord_rem_old_var_TS_TSEEEGCG.aov <- aov(V1 ~ genotype  + Error(id), data = new_coord_rem_old_var_TS_TSEEEGCG)
+summary(new_coord_rem_old_var_TS_TSEEEGCG.aov)
+
+
 # boxplot
 boxPlots_rem <- ggplot(new_coord_rem_TS , aes (genotype, V1, fill = genotype)) + 
   geom_boxplot(show_guide=FALSE) +
@@ -105,9 +114,7 @@ boxPlots_rem <- ggplot(new_coord_rem_TS , aes (genotype, V1, fill = genotype)) +
   labs(title = "Removal PC1\n") + xlab ("\ngentreat") + ylab("PC1\n") +
   theme (legend.title=element_blank())
 
-
 boxPlots_rem
-
 
 boxPlots_rem
 + 
@@ -117,7 +124,6 @@ boxPlots_rem
   scale_fill_manual(name = "Group", values = c("green", "black"), labels = c ("TS", "TSEEEGCG")) +
   #scale_fill_manual (name = "Group", values = c ("green", "brown")) +
   labs(title = "") + xlab ("\nAcquisition days") + ylab("PC1\n")
-
 
 plot(res,choix="var")
 cols=c("green","lightblue","black","orange","red","blue","darkgrey","magenta")
@@ -191,6 +197,96 @@ PCbiplot <- function(PC, x="PC1", y="PC2") {
 }
 
 PCbiplot(res_rem)
+
+
+#########################
+# I select for the pca the same variables than in the acquisition data of Ionas
+#########################
+
+selected_var_rem <- rem_data_all_var [,c(1:8,10,12,14,15,17,18)] 
+head (selected_var_rem)
+# Median with all available rem variables
+# tbl_stat_median <-with (rem_data_all_var, aggregate (cbind (NUMBER.ENTRIES, PERM.TIME, PERCENT.PERM.TIME, LATENCY.TARGET), list (GENTREAT), FUN=function (x) median=median(x)))
+tbl_stat_median <-with (selected_var_rem, aggregate (cbind (NUMBER.ENTRIES, PERM.TIME, LATENCY.TARGET, GALLINDEX.REM, SPEED.REM, PERC.NE.REM, PERC.PERI.REM, WISHAW.REM), list (GENTREAT), FUN=function (x) median=median(x)))                                                            
+genotype_tt <- as.factor(tbl_stat_median$Group.1)
+genotype_tt_ionas <- as.factor(c("WT","TS","WTEE","TSEE", "WTEGCG", "TSEGCG", "WTEEEGCG", "TSEEEGCG"))
+tbl_stat_median <- tbl_stat_median [,-1]
+PCA_rem <- prcomp(tbl_stat_median, scale=TRUE)
+summary(PCA_rem)
+
+# Plot PCA color by genotype
+pca_rem_2plot <- as.data.frame (PCA_rem$x)
+pca_rem_2plot$PC1_neg <- -pca_rem_2plot$PC1
+g_genotype_tt <- ggplot(pca_rem_2plot, aes(PC1_neg, PC2)) + geom_point(aes(colour=genotype_tt_ionas), size=4) +                                                           
+  labs (title = "PCA") +
+  #   scale_color_manual(values=cols, labels=c("1", "2", "3")) +
+  #   geom_text (aes (label=genotype_tt), hjust=0, vjust=-0.5)
+  geom_text (aes (label=genotype_tt_ionas), hjust=0.5, vjust=-0.5)+
+  xlim (c(-5, 5)) + ylim (c(-1.5,2))+
+  labs (title = "PCA removal") +  
+  labs (x = "\nPC1", y="PC2\n") +
+  theme (legend.key=element_rect(fill=NA), legend.title=element_blank())
+#, position=position_jitter(h=0), alpha = 1)
+
+g_genotype_tt
+
+#################################
+# Individual values for boxPlot
+# install.packages("FactoMineR")
+library(FactoMineR)
+selected_var_rem
+tbl_ind_rem <- selected_var_rem[,c(7:length(selected_var_rem[1,]))]
+tbl_ind_rem_med<- rbind (tbl_stat_median, tbl_ind_rem)
+res_pca_rem = PCA(tbl_ind_rem_med, scale.unit=TRUE, ind.sup=c(9:91))
+
+new_coord_rem <- cbind(res_pca_rem$ind.sup$coord[,1],res_pca_rem$ind.sup$coord[,2])
+new_coord_rem <- as.data.frame(new_coord_rem)
+# length(new_coord_rem$V1)
+# length(selected_var_rem$ID)
+
+new_coord_rem$genotype <- selected_var_rem[,c(6)]
+new_coord_rem$genotype <- gsub("H20", "", new_coord_rem$genotype)
+new_coord_rem$genotype <- gsub("NE", "", new_coord_rem$genotype)
+new_coord_rem$genotype <- factor(new_coord_rem$genotype , levels=c("WT", "TS", "WTEE", "TSEE", "WTEGCG", "TSEGCG", "WTEEEGCG", "TSEEEGCG"), 
+                                 labels=c("WT", "TS", "WTEE", "TSEE", "WTEGCG", "TSEGCG", "WTEEEGCG", "TSEEEGCG"))
+
+pca_plot_individuals_rem <- ggplot (data=new_coord_rem, aes (V1, V2)) + 
+  #   geom_text (aes(label=day, colour = genotype), size=3, show_guide = FALSE) +
+  geom_point () 
+
+#PLOT_paper
+ggsave (pca_plot_individuals, file=paste(home, "/20150515_PCA_old_frotiersPaper/figures/", "PCA_individuals.jpg", sep=""), dpi=900)
+
+# ANOVA
+# TS all treatments
+new_coord_rem_TS <- subset(new_coord_rem, grepl("TS", new_coord_rem$genotype))
+new_coord_rem_TS$id <- c(1:length(new_coord_rem_TS$V1))
+rem.aov <- aov(V1 ~ genotype  + Error(id), data = new_coord_rem_TS)
+summary(rem.aov)
+
+# TS vs TSEEEGCG 
+new_coord_rem_TS_TSEEEGCG <- subset(new_coord_rem_TS, genotype == "TS" | genotype == "TSEEEGCG")
+new_coord_rem_TS_TSEEEGCG$id <- c(1:length(new_coord_rem_TS_TSEEEGCG$V1))
+rem_TS_TSEEEGCG.aov <- aov(V1 ~ genotype  + Error(id), data = new_coord_rem_TS_TSEEEGCG)
+summary(rem_TS_TSEEEGCG.aov)
+
+pairwise.t.test(new_coord_rem_TS_TSEEEGCG$V1, new_coord_rem_TS_TSEEEGCG$genotype, , p.adj="hochberg", paired=F)
+
+# Comprobacion de las medias para ver porque no es significativo
+mean (new_coord_rem_TS_TSEEEGCG [ which(new_coord_rem_TS_TSEEEGCG$genotype=="TS"),"V1"])
+mean (new_coord_rem_TS_TSEEEGCG [ which(new_coord_rem_TS_TSEEEGCG$genotype=="TSEEEGCG"),"V1"])
+library (plotrix)
+std.error(new_coord_rem_TS_TSEEEGCG [ which(new_coord_rem_TS_TSEEEGCG$genotype=="TS"),"V1"])
+std.error (new_coord_rem_TS_TSEEEGCG [ which(new_coord_rem_TS_TSEEEGCG$genotype=="TSEEEGCG"),"V1"])
+
+stderr(new_coord_rem_TS_TSEEEGCG [ which(new_coord_rem_TS_TSEEEGCG$genotype=="TSEEEGCG"),"V1"])
+boxPlots_rem <- ggplot(new_coord_rem_TS_TSEEEGCG , aes (genotype, V1, fill = genotype)) + 
+  geom_boxplot(show_guide=FALSE) +
+  scale_fill_manual(name = "genotype", values=c("green", "black")) +
+  labs(title = "Removal PC1\n") + xlab ("\ngentreat") + ylab("PC1\n") +
+  theme (legend.title=element_blank())
+
+boxPlots_rem
 
 # textxy(res_rem$x[2,1],res_rem$x[2,2], row.names(res_rem$x)[1], cex=0.7,col="green")
 # lines(res_rem$x[2,1], res_rem$x[2,2],col="black")
