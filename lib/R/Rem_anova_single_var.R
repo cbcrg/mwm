750ed1b59c6682ee4e936427ecdc42a9cb85ea52
Jose Espinosa <kadomu@gmail.com>
2015-07-19 18:03:23 +0200

    Plot of removal gallagher index with outlier signed by a triangle

--- lib/R/Rem_anova_single_var.R
+++ lib/R/Rem_anova_single_var.R
@@ -111,13 +111,38 @@ bp_gallagher <- ggplot(rem_data_all_var , aes (genotype, GALLINDEX.REM, fill = g
                colour="white", size =0.8)
 
 bp_gallagher_points <- bp_gallagher + geom_point (position = position_jitter(width = 0.2), colour="red", show_guide=FALSE)
-bp_gallagher_points 
-
 
 #PLOT_paper
 ggsave (bp_gallagher_points, file=paste(home, "/20150515_PCA_old_frotiersPaper/figures/", "rem_gallagher_all_gr.jpg", sep=""), 
         width=14, height=7, dpi=900)
 
+### Gallagher index without mouse = "130054742" that is considered an outlier according to JR analysis
+# Adding mice ID
+geom_text (data=neg_positions, aes (x=-Dim.1, y=-Dim.2, label=neg_labels, hjust=1.2), show_guide = FALSE, size=5) + 
+outlier <- rem_data_all_var [which (rem_data_all_var$ID == "130054742"),]
+
+# Plot showing the outlier
+bp_gallagher_points + geom_text(data=outlier , aes (label=ID))
+
+#Removing the individual from the analysis to make the boxplot
+rem_data_all_var_noOutlier <- rem_data_all_var [-which (rem_data_all_var$ID == "130054742"),]
+bp_gallagher_noOut <- ggplot(rem_data_all_var_noOutlier , aes (genotype, GALLINDEX.REM, fill = genotype)) + 
+  geom_boxplot (outlier.size=NA, show_guide=FALSE) +
+  scale_fill_manual(name = "genotype", values = c("red", "darkgreen", "blue", "lightblue", "magenta", "orange", "yellow", "black")) +
+  labs(title = "Removal gallagher index\n") + xlab ("\ngentreat") + ylab("Gallagher index (cm)\n") +
+  theme (legend.title=element_blank()) +
+  geom_segment(aes(x = 7.63, y = median(rem_data_all_var [rem_data_all_var$genotype == "TSEEEGCG","GALLINDEX.REM"]), 
+                   xend = 8.37, yend = median(rem_data_all_var [rem_data_all_var$genotype == "TSEEEGCG","GALLINDEX.REM"])), 
+               colour="white", size =0.8) + 
+              geom_point (position = position_jitter(width = 0.2), colour="red", show_guide=FALSE)
+
+bp_gallagher_noOut <- bp_gallagher_noOut + geom_point(data=outlier , aes (label=ID), show_guide=FALSE, shape = 2)
+
+#PLOT_paper
+ggsave (bp_gallagher_noOut, file=paste(home, "/20150515_PCA_old_frotiersPaper/figures/", "rem_gallagher_all_gr_outlier.jpg", sep=""), 
+        width=14, height=7, dpi=900)
+
+# Only trisomic group
 bp_rem_gallagher_TS <- ggplot(rem_data_all_var_TS , aes (genotype, GALLINDEX.REM, fill = genotype)) + 
   geom_boxplot(show_guide=FALSE) +
 #   geom_text (aes (label=ID), vjust=-0.5, hjust=1, size=4, show_guide = T)
