6a9de617cf0a359b0feb1da6a8cf49d7b3635de1
Jose Espinosa <kadomu@gmail.com>
2016-02-22 10:49:47 +0100

    Reversal panel pca created using cowplot

--- lib/R/PCA_MWM_youngTs65_reversal.R
+++ lib/R/PCA_MWM_youngTs65_reversal.R
@@ -12,6 +12,10 @@ library("ggplot2")
 library("Hmisc")
 library("FactoMineR") #PCA
 
+size_titles <- 18
+size_axis <- 14
+size_strips <- 12
+dpi_q <- 300 
 
 #############
 ### Latency old
@@ -24,6 +28,7 @@ head (smart_data)
 # head (data_nxf)
 # Loading functions:
 source (paste (home, "/git/mwm/lib/R/plot_param_public.R", sep=""))
+source (paste (home, "/git/mwm/lib/R/stat_density_2d_function.R", sep=""))
 
 # The last records are empty
 tail(jtracks_data, 50)
@@ -77,9 +82,9 @@ tbl_ind <- young_rev_7var
 tbl_med_ind <- rbind (tbl_median, tbl_ind[,-1])
 n_median <- length(tbl_median[,1])
 n_median_plus1 <- n_median + 1
-
+pca_medians_rev_aspect_ratio
 res = PCA(tbl_med_ind[,(3:9)], scale.unit=TRUE, ind.sup=c(n_median_plus1:length(tbl_med_ind[,1]))) 
-
+pca_medians_rev_aspect_ratio
 # res$var$coord[,1],res$var$coord[,2]
 summary_resPCA<- summary(res)
 
@@ -101,12 +106,12 @@ pca2plot$gentreat <- factor(pca2plot$gentreat , levels=c("WT", "TS", "WTEEEGCG",
 
 ## To have the same direction than the old mice reversal I change the direction on only the X axis
 pca_medians_rev <- ggplot(pca2plot, aes(x=-Dim.1, y=Dim.2, colour=gentreat )) + 
-  #                           geom_path (size = 1,show_guide = T) + 
-                          geom_path (size = 1, show_guide = F) + 
+  #                           geom_path (size = 1,show.legend = T) + 
+                          geom_path (size = 1, show.legend = F) + 
                           scale_color_manual (values=c("red", "darkgreen", "magenta", "black")) +
                           
-                          #                           geom_text (aes (label=days), vjust=-0.5, hjust=1, size=4, show_guide = T)+
-                          geom_text (aes (label=days), vjust=-0.5, hjust=1, size=4, show_guide = F)+
+                          #                           geom_text (aes (label=days), vjust=-0.5, hjust=1, size=4, show.legend = T)+
+                          geom_text (aes (label=days), vjust=-0.5, hjust=1, size=4, show.legend = F)+
                           theme (legend.key=element_rect(fill=NA)) +
                           labs (title = "PCA of group medians\n", x = paste("\nPC1 (", var_PC1, "% of variance)", sep=""), 
                                 y=paste("PC2 (", var_PC2, "% of variance)\n", sep = "")) +
@@ -122,6 +127,11 @@ pca_medians_rev_aspect_ratio <- pca_medians_rev + coord_fixed() +
                                                   scale_x_continuous (limits=c(-5, 5), breaks=-5:5) + 
                                                   scale_y_continuous (limits=c(-2.5, 2.5), breaks=-2:2)
 
+pca_medians_rev_aspect_ratio <- pca_medians_rev_aspect_ratio + 
+                                panel_border() + theme(panel.border = element_rect(colour = "black")) + 
+                                theme(plot.title = element_text(size=size_titles)) + 
+                                theme(axis.title.x = element_text(size=size_axis)) +
+                                theme(axis.title.y = element_text(size=size_axis)) + coord_fixed()
 pca_medians_rev_aspect_ratio
 # ggsave (pca_medians_rev_aspect_ratio, file=paste(home, "/20151001_ts65_young_MWM/figures/fig_reversal/", 
 #         "PCA_medians_legend_rev.jpg", sep=""), width = 10, height = 6, dpi=900)
@@ -147,15 +157,20 @@ df.circle <- data.frame(x = sin(angle), y = cos(angle))
 ## To have the same direction than the old mice reversal I change the direction on only the X axis  
 p_circle_plot <- ggplot(circle_plot) + 
   geom_segment (data=circle_plot, aes(x=0, y=0, xend=-Dim.1, yend=Dim.2), arrow=arrow(length=unit(0.2,"cm")), alpha=1, size=1, color="red") +
-  xlim (c(-1.2, 1.2)) + ylim (c(-1.2, 1.2)) +
-  geom_text (data=neg_positions, aes (x=-Dim.1, y=Dim.2, label=neg_labels, hjust=1.2), show_guide = FALSE, size=5) + 
-  geom_text (data=pos_positions, aes (x=-Dim.1, y=Dim.2, label=pos_labels, hjust=-0.3), show_guide = FALSE, size=5) +
+  scale_x_continuous(limits=c(-1.8, 1.8), breaks=(c(-1,0,1))) +
+  scale_y_continuous(limits=c(-1.8, 1.8), breaks=(c(-1,0,1))) +
+  geom_text (data=neg_positions, aes (x=-Dim.1, y=Dim.2, label=neg_labels, hjust=1.2), show.legend = FALSE, size=5) + 
+  geom_text (data=pos_positions, aes (x=-Dim.1, y=Dim.2, label=pos_labels, hjust=-0.3), show.legend = FALSE, size=5) +
   geom_vline (xintercept = 0, linetype="dotted") +
   geom_hline (yintercept=0, linetype="dotted") +
   labs (title = "PCA of the variables\n", x = paste("\nPC1 (", var_PC1, "% of variance)", sep=""), 
         y=paste("PC2 (", var_PC2, "% of variance)\n", sep = "")) +
   geom_polygon (data = df.circle, aes(x, y), alpha=1, colour="black", fill=NA, size=1)
 
+p_circle_plot <- p_circle_plot + panel_border() + theme(panel.border = element_rect(colour = "black")) + 
+                 theme(plot.title = element_text(size=size_titles)) + 
+                 theme(axis.title.x = element_text(size=size_axis)) +
+                 theme(axis.title.y = element_text(size=size_axis)) + coord_fixed()
 p_circle_plot
 
 # ggsave (p_circle_plot, file=paste(home, "/20151001_ts65_young_MWM/figures/fig_reversal/", "circle_plot.jpg", sep=""), width = 10, height = 10, dpi=900)
@@ -175,7 +190,10 @@ bars_plot <- ggplot (data=df.bars_to_plot, aes(x=index, y=value)) +
                      geom_bar (stat="identity", fill="gray", width=0.8) + 
                      labs (title = "Variable contribution to PC1\n", x = "", y="Contribution in %\n") +
                      theme(axis.text.x=element_text(angle=45, vjust=1, hjust=1) )
-bars_plot
+bars_plot <- bars_plot + panel_border() + theme(panel.border = element_rect(colour = "black")) +
+             theme(plot.title = element_text(size=size_titles)) + 
+             theme(axis.title.x = element_text(size=size_axis)) +
+             theme(axis.title.y = element_text(size=size_axis))
 
 #PLOT_paper
 # ggsave (bars_plot, file=paste(home, "/20151001_ts65_young_MWM/figures/fig_reversal/", "bar_contribution.jpg", sep=""), dpi=900)
@@ -195,11 +213,32 @@ bars_plot_PC2 <- ggplot (data=df.bars_to_plot_PC2, aes(x=index, y=value)) +
   geom_bar (stat="identity", fill="gray", width=0.8) + 
   labs (title = "Variable contribution to PC2\n", x = "", y="Contribution in %\n") +
   theme(axis.text.x=element_text(angle=45, vjust=1, hjust=1) )
+bars_plot_PC2 <- bars_plot_PC2 + panel_border() + theme(panel.border = element_rect(colour = "black"))
+
+bars_plot_PC2 <- bars_plot_PC2 + panel_border() + theme(panel.border = element_rect(colour = "black")) +
+                 theme(plot.title = element_text(size=size_titles)) + 
+                 theme(axis.title.x = element_text(size=size_axis)) +
+                 theme(axis.title.y = element_text(size=size_axis))
+
 bars_plot_PC2
 
 #PLOT_paper
 # Final version
 # ggsave (bars_plot_PC2, file=paste(home, "/20151001_ts65_young_MWM/figures/fig_reversal/", "bar_contribution_PC2.jpg", sep=""), dpi=900)
+
+#################
+#######
+## Panel
+panel_pca <- ggdraw() + draw_plot (pca_medians_rev_aspect_ratio, 0, .5, 0.5, .5) +
+  draw_plot (p_circle_plot, 0.5, 0.5, 0.5, 0.5) +
+  draw_plot (bars_plot, 0, 0, 0.5, .5) +
+  draw_plot (bars_plot_PC2, 0.5, 0, 0.5, .5) +
+  draw_plot_label(c("A", "B", "C"), c(0, 0.5, 0), c(1, 1, 0.5), size = size_titles)
+panel_pca
+
+img_format=".tiff"
+ggsave (panel_pca, file=paste(home, "/20151001_ts65_young_MWM/figures/fig_reversal/", "panel_PCA_reversal", img_format, sep=""), 
+        dpi=dpi_q, width=15, height=11)
 
 ###################################
 # Plot of supplementary individuals
@@ -218,26 +257,26 @@ new_coord$genotype <- factor(new_coord$genotype , levels=c("WT", "TS", "WTEEEGCG
                              labels=c("WT", "TS", "WTEEEGCG", "TSEEEGCG"))
 
 pca_plot_individuals <- ggplot (data=new_coord, aes (V1, V2)) + 
-  geom_text (aes(label=day, colour = genotype), size=5, show_guide = FALSE) +
+  geom_text (aes(label=day, colour = genotype), size=5, show.legend = FALSE) +
   scale_color_manual(values=c("red", "darkgreen", "magenta", "black")) +
   xlim (c(-6, 6)) + ylim (c(-6, 6)) +
-  geom_path (data=pca2plot, aes(x=-Dim.1, y=Dim.2, colour=gentreat),size = 1,show_guide = FALSE) +
+  geom_path (data=pca2plot, aes(x=-Dim.1, y=Dim.2, colour=gentreat),size = 1,show.legend = FALSE) +
   labs(title = "Individual as supplementary points\n", x = paste("\nPC1 (", var_PC1, "% of variance)", sep=""), 
        y=paste("PC2 (", var_PC2, "% of variance)\n", sep = ""))  +
   coord_fixed()
 
-pca_plot_individuals
+pca_plot_individuals <- pca_plot_individuals + panel_border() + theme(panel.border = element_rect(colour = "black"))
 
 #PLOT_paper
-ggsave (pca_plot_individuals, file=paste(home, "/20151001_ts65_young_MWM/figures/fig_reversal/", "PCA_individuals.jpg", sep=""),
-        height = 10, width = 10, dpi=900)
+# ggsave (pca_plot_individuals, file=paste(home, "/20151001_ts65_young_MWM/figures/fig_reversal/", "PCA_individuals.jpg", sep=""),
+#         height = 10, width = 10, dpi=900)
 
 head(new_coord)
 ## Individual variation as density plots
 p_cloud_indiv_by_day <- ggplot(new_coord, aes(V1, V2, color=genotype, label=day)) + 
   stat_density2d(aes(fill=factor(genotype), alpha = ..level..), 
-                 geom="polygon", color=NA, n=100, h=4, bins=6, show_guide = FALSE) + 
-  geom_point(show_guide = FALSE) + 
+                 geom="polygon", color=NA, n=100, h=4, bins=6, show.legend = FALSE) + 
+  geom_point(show.legend = FALSE) + 
   scale_color_manual(name='genotype', 
                      values = c("red", "darkgreen", "magenta", "black"),                                            
                      labels = c("WT", "TS", "WTEEEGCG", "TSEEEGCG")) +
@@ -256,7 +295,7 @@ p_cloud_indiv_by_day <- ggplot(new_coord, aes(V1, V2, color=genotype, label=day)
   labs(title = "Density plot of individual variation\n", x = "\nPC1", y="PC2\n") +
   scale_alpha_continuous(range=c(0.3,0.5)) 
 # +
-#   geom_path (data=pca2plot, aes(x=Dim.1, y=Dim.2, colour=gentreat), size = 0.5, linetype = 2, show_guide = FALSE)
+#   geom_path (data=pca2plot, aes(x=Dim.1, y=Dim.2, colour=gentreat), size = 0.5, linetype = 2, show.legend = FALSE)
 
 p_cloud_indiv_by_day
 p_cloud_indiv_by_day_facet <- p_cloud_indiv_by_day + facet_wrap(~genotype, ncol = 2)
@@ -273,10 +312,13 @@ PC1_rev1_3 <- subset(new_coord, day %in% c("1", "3"), c("V1", "V2", "day","genot
 colnames (PC1_rev1_3) <- c("PC1","PC2", "day", "genotype_tt") 
 PC1_rev1_3$day <- as.factor(PC1_rev1_3$day)
 
+## Labels facet
+levels(PC1_rev1_3$day) <- c("Session 1","Session 3")
+
 p_cloud_rev1_3 <- ggplot(PC1_rev1_3, aes(PC1, PC2, color=genotype_tt)) + 
   stat_density2d(aes(fill=factor(genotype_tt), alpha = ..level..), 
-                 geom="polygon", color=NA, n=100, h=5, bins=6, show_guide = F) +
-  geom_point(show_guide = F) + 
+                 geom="polygon", color=NA, n=100, h=5, bins=6, show.legend = F) +
+  geom_point(show.legend = F) + 
   scale_color_manual(name='genotype_tt', 
                      values = c("red", "darkgreen", "magenta", "black"),                                            
                      labels = c("WT", "TS", "WTEEEGCG", "TSEEEGCG")) +
@@ -292,8 +334,16 @@ p_cloud_rev1_3 <- ggplot(PC1_rev1_3, aes(PC1, PC2, color=genotype_tt)) +
 
 p_cloud_rev1_3_facet <- p_cloud_rev1_3 + facet_grid(day ~ genotype_tt, margins=FALSE) + geom_vline(xintercept = 0, colour="gray") +
   geom_hline(yintercept = 0, colour="gray")
-p_cloud_rev1_3_facet
-
+p_cloud_rev1_3_facet <- p_cloud_rev1_3_facet + panel_border() + theme(panel.border = element_rect(colour = "black")) +
+                        theme(plot.title = element_text(size=size_titles)) + 
+                        theme(axis.title.x = element_text(size=size_axis)) +
+                        theme(axis.title.y = element_text(size=size_axis)) +
+                        
+p_cloud_rev1_3_facet_strips <-  p_cloud_rev1_3_facet + theme(strip.text.x = element_text(size=size_strips, face="bold"), strip.text.y = element_text(size=size_strips, face="bold", angle=90)) +
+                                theme(plot.title = element_text(size=size_titles)) + 
+                                theme(axis.title.x = element_text(size=size_axis)) +
+                                theme(axis.title.y = element_text(size=size_axis)) +
+                                theme(strip.background = element_blank()) 
 #PLOT_presentation
 # ggsave (p_cloud_rev1_3_facet, file=paste(home, "/20151001_ts65_young_MWM/figures/", "PCA_density_byGroupAndDay.jpg", sep=""), 
 #         width = 10, height = 6, dpi=900)
@@ -302,11 +352,13 @@ p_cloud_rev1_3_facet
 # I can use the same table
 PC1_rev1_3$genotype <- PC1_rev1_3$genotype_tt
 
-PC1.rev1 <- subset(PC1_rev1_3,  day==1)
-PC1.rev3 <- subset(PC1_rev1_3,  day==3)
+# PC1.rev1 <- subset(PC1_rev1_3,  day==1)
+# PC1.rev3 <- subset(PC1_rev1_3,  day==3)
+PC1.rev1 <- subset(PC1_rev1_3,  day=="Session 1")
+PC1.rev3 <- subset(PC1_rev1_3,  day=="Session 3")
 
 boxPlots.PC1.rev1 <- ggplot(PC1.rev1, aes (genotype, PC1, fill = genotype)) + 
-  geom_boxplot(show_guide=FALSE) +
+  geom_boxplot(show.legend=FALSE) +
   scale_fill_manual(name = "Genotype", values=c("red", "darkgreen", "magenta", "black")) +
   labs(title = "Session 1 PC1\n") + xlab ("\nGroups") + ylab("PC1\n") +
   theme (legend.title=element_blank()) + 
@@ -322,7 +374,7 @@ boxPlots.PC1.rev1.line
 
 ## Session 3
 boxPlots.PC1.rev3 <- ggplot(PC1.rev3, aes (genotype, PC1, fill = genotype)) + 
-  geom_boxplot(show_guide=FALSE) +
+  geom_boxplot(show.legend=FALSE) +
   scale_fill_manual(name = "Genotype", values=c("red", "darkgreen", "magenta", "black")) +
   labs(title = "Session 3 PC1\n") + xlab ("\nGroups") + ylab("PC1\n") +
   theme (legend.title=element_blank()) + 
@@ -339,7 +391,55 @@ max(PC1.rev3$PC1)
 #PLOT_paper
 # ggsave (boxPlots.PC1.rev1.line, file=paste(home, "/20151001_ts65_young_MWM/figures/fig_reversal/", "boxPlot_PC1_rev1.jpg", sep=""), dpi=900)
 # ggsave (boxPlots.PC1.rev3.line, file=paste(home, "/20151001_ts65_young_MWM/figures/fig_reversal/", "boxPlot_PC1_rev3.jpg", sep=""), dpi=900)
- 
+
+################
+#### BOXPLOTS facet
+PC1_rev1_3$genotype <- PC1_rev1_3$genotype_tt
+
+median_line_r1 <- data.frame(x=3.63,y=median(PC1.rev1[PC1.rev1$genotype == "TSEEEGCG","PC1"]),
+                             xend=4.37, yend = median(PC1.rev1[PC1.rev1$genotype == "TSEEEGCG","PC1"]),
+                             day=factor("Session 1", levels=c("Session 1","Session 3")), genotype="TS")
+
+median_line_r3 <- data.frame(x=3.63,y= median(PC1.rev3[PC1.rev3$genotype == "TSEEEGCG","PC1"]),
+                             xend=4.37, yend= median(PC1.rev3[PC1.rev3$genotype == "TSEEEGCG","PC1"]),
+                             day=factor("Session 3", levels=c("Session 1","Session 3")), genotype="TS")
+
+# boxPlots.PC1.facet <- ggplot(PC1_acq1_5, aes (genotype, PC1, fill = genotype)) + 
+#   geom_boxplot(show.legend=FALSE) +
+#   #   scale_fill_manual(name = "Genotype", values=c("red", "darkgreen", "magenta", "black", "gray")) +
+#   scale_fill_manual(name = "Genotype", values=c("darkgreen", "black","magenta", "red", "magenta")) +
+#   labs(title = "PC1 distribution\n") + xlab ("\nGroups") + ylab("PC1\n") +
+#   theme (legend.title=element_blank()) + 
+#   # Same axis limits in day 1 and day 5
+#   #   scale_y_continuous(breaks=c(-4,-2,0,2,4,6,8), limits=c(-6, 0.5)) +
+#   scale_y_continuous(breaks=c(-4,-2,0,2,4,6,8,10,12), limits=c(-4.5, 13)) +
+#   geom_segment(data=median_line_5, aes(x = x, y = y, xend=xend, yend=yend), colour="white") +
+#   geom_segment(data=median_line_1, aes(x = x, y = y, xend=xend, yend=yend), colour="white") +
+#   facet_grid(. ~ day)
+#  
+boxPlots.PC1.rev.facet <- ggplot(PC1_rev1_3, aes (genotype, PC1, fill = genotype)) + 
+  geom_boxplot(show.legend=FALSE) +
+  scale_fill_manual(name = "Genotype", values=c("red", "darkgreen", "magenta", "black")) +
+  labs(title = "Session 1 PC1\n") + xlab ("\nGroups") + ylab("PC1\n") +
+  theme (legend.title=element_blank()) + 
+  # Same axis limits in day 1 and day 5
+  #   scale_y_continuous(breaks=c(-4,-2,0,2,4,6,8), limits=c(-6, 0.5)) +
+  #   scale_y_continuous(breaks=c(-4,-2,0,2,4,6,8), limits=c(-5.5, 9.5)) +
+  scale_y_continuous(breaks=c(-6,-4,-2,0,2,4,6), limits=c(-6, 6)) +
+  geom_segment(data=median_line_r3, aes(x = x, y = y, xend=xend, yend=yend), colour="white") +
+  geom_segment(data=median_line_r1, aes(x = x, y = y, xend=xend, yend=yend), colour="white") +
+  facet_grid(. ~ day) +
+  geom_hline(yintercept = 0, colour="gray")
+
+
+
+# p_cloud_indiv_by_day_facet <- boxPlots.PC1 + facet_wrap(~genotype, ncol = 2)
+boxPlots.PC1.rev3.line <- boxPlots.PC1.rev3 + geom_hline(yintercept = 0, colour="gray")
+boxPlots.PC1.rev3.line
+max(PC1.rev3$PC1)
+#####################
+####################
+###################
 
 # Plotting a legend with scuares and colors
 l <- ggplot() + geom_point(data=PC1.rev3, aes (x=PC1, y=PC2, colour = genotype), shape=15, size=5) +
